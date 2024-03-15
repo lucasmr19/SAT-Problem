8e36ce5a99a6f4ff3ca0a02f35f1a8dfb34209d1
lucasmr19 <157109722+lucasmr19@users.noreply.github.com>
2024-03-15 13:52:32 +0100

    Update SATMultiThread.java

--- src/cnf/SATMultiThread.java
+++ src/cnf/SATMultiThread.java
@@ -1,132 +1,124 @@
 package cnf;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.ExecutorCompletionService;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class SATMultiThread implements Callable<LocalSearchSolution> {
-    // Global variables for all the threads:
-    private CNF F; 
+public class SATMultiThread implements Runnable {
+    private int numVars_F;
+    private CNF F;
     private int maxTries;
     private int maxFlips;
-    private static int numVars_F;
-    private static int num_empty_clauses;
-    private static int best_false_count = Integer.MAX_VALUE;
-    private static int[] best_configuration;
-    private static int tryCount = 0;
-    private static int ultimate_flipCount;
-    private Lock lock = new ReentrantLock();
-    private static boolean shouldStop = false; // Flag to stop the execution
+    private GlobalVariables globalVars;
+    private Lock lock;
 
-    public SATMultiThread(CNF F, int maxTries, int maxFlips, int numVars_F) {
+    
+
+    public SATMultiThread(CNF F, int maxTries, int maxFlips, int numVars_F, GlobalVariables globalVars, Lock lock) {
         this.F = F;
         this.maxTries = maxTries;
         this.maxFlips = maxFlips;
-        SATMultiThread.numVars_F = numVars_F;
+        this.numVars_F = numVars_F;
+        this.globalVars = globalVars;
+        this.lock = lock;
     }
 
+
     @Override
-    public LocalSearchSolution call() {
-        // Local variables:
+    public void run() {
+        // Local vars
         int[] random_configuration = RandomConfigGenerator.getRandomConfig(numVars_F);
         int[] new_configuration = random_configuration.clone();
+        int[] best_configuration_local = random_configuration.clone();
 
-        int false_count = F.eval(random_configuration);
-        int flipCount = 0;
-        lock.lock();
-        try {
-            updateBestSolution(new_configuration, false_count, flipCount);
-        } finally {
-            lock.unlock();
-        }
-        while (tryCount < maxTries && best_false_count != num_empty_clauses && !shouldStop) {
+
+        int false_count;
+        int best_false_count_local = numVars_F; // Big number for initialize
+        int flipCount;
+        int i;
+
+        this.lock.lock();
+        while (globalVars.getTryCount() < maxTries && !globalVars.getShouldStop()) {
+            
+            globalVars.incTryCount();
+            this.lock.unlock();
+
             flipCount = 0;
             new_configuration = random_configuration.clone();
-            while (flipCount < maxFlips && best_false_count != num_empty_clauses && !shouldStop) {
-                int i = 0;
-                while (i < numVars_F && best_false_count != num_empty_clauses && !shouldStop) {
+            while (flipCount < maxFlips && !globalVars.getShouldStop()) {
+                i = 0;
+                while (i < numVars_F && !globalVars.getShouldStop()) {
                     new_configuration[i] *= -1;
                     false_count = F.eval(new_configuration);
-                    lock.lock();
-                    try {
-                        updateBestSolution(new_configuration, false_count, flipCount);
-                    } finally {
-                        lock.unlock();
+                    if (false_count < best_false_count_local && !globalVars.getShouldStop()) {
+                        best_false_count_local = false_count;
+                        best_configuration_local = new_configuration.clone();
+                        updateGlobalVars(new_configuration, false_count, flipCount);
                     }
                     new_configuration[i] *= -1;
                     i++;
                 }
-                new_configuration = best_configuration.clone();
-                false_count = best_false_count;
+                new_configuration = best_configuration_local.clone();
+                false_count = best_false_count_local;
                 flipCount++;
             }
             random_configuration = RandomConfigGenerator.getRandomConfig(numVars_F);
-            lock.lock();
+            this.lock.lock();
+        }
+        this.lock.unlock();
+    }
+
+    private void updateGlobalVars(int[] new_configuration, int false_count, int flipCount) {
+        globalVars.updateGlobalVars(new_configuration, false_count, flipCount);
+    }
+    
+    public static LocalSearchSolution solveSATMultiThread(CNF F, int maxTries, int maxFlips, int numThreads) throws IllegalArgumentException, InterruptedException {
+        if (F.getNumEmptyClauses() != 0) {
+            return new LocalSearchSolution(null, 0, 0);
+        }
+
+        int numVars_F = F.getNumVars();
+        GlobalVariables globalVars = new GlobalVariables();
+        Lock lock = new ReentrantLock();
+
+        List<Thread> threads = new ArrayList<Thread>();
+
+        for (int i = 1; i < numThreads +1 ; i++) {
+            Thread thread = new Thread(new SATMultiThread(F, maxTries, maxFlips, numVars_F, globalVars, lock));
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
             try {
-                tryCount++;
-            } finally {
-                lock.unlock();
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
-        if (best_false_count != 0) {
+
+        if (globalVars.getBest_false_count() != 0) {
             return new LocalSearchSolution(null, maxTries, maxFlips);
         } else {
-            return new LocalSearchSolution(best_configuration, tryCount, ultimate_flipCount);
-        }
-    }
-    // Encapsulating the update logic of global variables: best_false_count, best_configuration & shoulStop
-    private void updateBestSolution(int[] configuration, int false_count, int flipCount) {
-        if (false_count < best_false_count && !shouldStop) {
-            best_false_count = false_count;
-            best_configuration = configuration.clone();
-            if (best_false_count == 0) {
-                shouldStop = true;
-                ultimate_flipCount = flipCount;
-            }
-        }
-    }
-
-    public static LocalSearchSolution solveSATMultiThread(CNF F, int maxTries, int maxFlips, int numThreads) throws Exception {
-        numVars_F = F.getNumVars();
-        num_empty_clauses = F.getNumEmptyClauses();
-        ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
-        CompletionService<LocalSearchSolution> completionService = new ExecutorCompletionService<>(executorService);
-
-        for (int i = 0; i < numThreads; i++) {
-            Callable<LocalSearchSolution> task = new SATMultiThread(F, maxTries, maxFlips, numVars_F);
-            completionService.submit(task); // Start each Thread
-            
-        }
-      
-        for (int i = 0; i < numThreads; i++) {
-        completionService.take(); // Wait till stop each Thread
-        }
-
-        executorService.shutdown();
-
-        if (best_false_count != 0) {
-            return new LocalSearchSolution(null, maxTries, maxFlips);
-        } else {
-            return new LocalSearchSolution(best_configuration, tryCount, ultimate_flipCount);
+            return new LocalSearchSolution(globalVars.getBest_configuration(), globalVars.getTryCount(), globalVars.getUltimate_flipCount());
         }
     }
 
     public static void main(String[] args) {
         try {
             String dir = "data/";
-            CNF F = CNF.readCNFFromFile(dir, "ejemplo_9.cnf");
-            LocalSearchSolution result = solveSATMultiThread(F, 50, 20, 4);
+            CNF F = CNF.readCNFFromFile(dir, "example_7.cnf");
 
-            // Mostrar los resultados
-            System.out.println(F);
-            System.out.println("Local Result: " + Arrays.toString(result.solution));
+            LocalSearchSolution result = solveSATMultiThread(F, 5, 20, 4);
+
             System.out.println("numTries: " + result.numTries);
             System.out.println("numFlips: " + result.numFlips);
+            if (result.solution == null){
+                System.out.println("No solution found :(");
+            } else {
             System.out.println("Evaluating CNF...: " + F.eval(result.solution));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
